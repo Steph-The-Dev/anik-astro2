3dd06065ef3a54dbc3c5efb9956bdaf7a325c7e2
Steph-The-Dev <69237947+Steph-The-Dev@users.noreply.github.com>
2024-04-28 16:18:10 +0200

    new colors and fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -79,4 +79,21 @@ export default {
       950: 'var(--accent-950)',
     },
   },
+  fontSize: {
+    sm: '0.750rem',
+    base: '1rem',
+    xl: '1.333rem',
+    '2xl': '1.777rem',
+    '3xl': '2.369rem',
+    '4xl': '3.158rem',
+    '5xl': '4.210rem',
+  },
+  fontFamily: {
+    heading: 'Cormorant',
+    body: 'Proza Libre',
+  },
+  fontWeight: {
+    normal: '400',
+    bold: '700',
+  },
 }
