7e91607cd8e6e050ba2f68a697c00eb8aade928e
Steph-The-Dev <69237947+Steph-The-Dev@users.noreply.github.com>
2024-08-04 11:03:36 +0200

    added styling sys beratung

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,8 +1,15 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro"],
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  trailingComma: "none",
   overrides: [
+    {
+      files: "*.svg",
+      options: {
+        parser: "html",
+      },
+    },
     {
       files: "*.astro",
       options: {
