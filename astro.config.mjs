f9b6c1169bf44263e1be3df8adfa6e80125e06c2
Steph-The-Dev <69237947+Steph-The-Dev@users.noreply.github.com>
2025-02-17 13:18:36 +0100

    navbar footer in layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from "astro/config";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
 
 import alpinejs from "@astrojs/alpinejs";
 
@@ -6,7 +8,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs()],
+  site: "https://www.anikvonrohr.de",
+  integrations: [alpinejs(), robotsTxt(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
